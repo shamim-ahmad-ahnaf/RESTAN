353a943a3499e6ef027af5830407621122101e3f
SHAMIM AHMAD <shamimahmadahnaf@gmail.com>
2024-12-26 00:35:44 +0600

    Add files via upload

---
+++ projeckt-3/assets/js/custom.js
@@ -0,0 +1,23 @@
+
+
+let menuToggle = document.getElementById('menu-toggle');
+let menuList = document.getElementById('menu-list');
+
+menuToggle.addEventListener('click', function() {
+    menuList.classList.toggle('active');    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
